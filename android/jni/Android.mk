2f02a63dff3b9b8019224cb04f01db6b0ea0bf48
ogcraft <ogcraft@gmail.com>
2014-03-26 21:01:38 +0200

    add boost

--- android/jni/Android.mk
+++ android/jni/Android.mk
@@ -8,7 +8,7 @@ LOCAL_CFLAGS := -O3
 LOCAL_CPPFLAGS :=$(LOCAL_CFLAGS)
 
 LOCAL_SRC_FILES := amatch_interface_imp.cpp \
-opensl_io2.c \
+opensl_io2.cpp \
 amatch_main.cpp \
 amatch_interface.cpp \
 ../../src/amatch.cpp \
@@ -21,6 +21,13 @@ amatch_interface.cpp \
 #LOCAL_SHARED_LIBRARIES := -L$(LOCAL_PATH)/../ext_libs/armabi -lsndfile
 
 #LOCAL_SHARED_LIBRARIES := -lsndfile
+
+LOCAL_CFLAGS += -I$(LOCAL_PATH)/boost/include/boost-1_54
+LOCAL_LDLIBS += -L$(LOCAL_PATH)/boost/lib/ -lboost_system 
+
+LOCAL_CPPFLAGS += -fexceptions -fpermissive
+LOCAL_CPPFLAGS += -frtti
+
 LOCAL_LDLIBS := -llog -lOpenSLES 
 
 include $(BUILD_SHARED_LIBRARY)
